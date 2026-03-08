0d40f45f85c5f7e65efe9ad5a48a871fb5548906
Kirannn <kaurkirannjot@gmail.com>
2026-03-09 01:00:42 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName="fitness-cache-v1"
+
+const files=[
+"/",
+"/index.html",
+"/manifest.json"
+]
+
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+caches.open(cacheName).then(cache=>{
+return cache.addAll(files)
+})
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res || fetch(e.request)
+})
+)
+
+})
